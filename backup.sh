241a1b582c6ddd4ececa0772452d81685b1b7a57
shazib96 <shaojoiya@gamil.com>
2022-12-27 07:28:04 +0000

    add script for backup

--- backup.sh
+++ backup.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date +%Y-%m-%d-%H%M%S)
 BACKUP_DIR="/home/backup"
-SOURCE="/var/lib/jenkins/workspace/test-deployment/*"
+SOURCE="var/lib/jenkins/workspace/test-deployment/*"
 tar -cvzpf $BACKUP_DIR/backup-$DATE.tar.gz $SOURCE
